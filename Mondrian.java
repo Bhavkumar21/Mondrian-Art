291d3d7d4794bc74ad1e61b13ebf42f62d20c4c9
Bhavesh Kumar <bhavesh@Bhaveshs-MacBook-Pro.local>
2023-02-15 16:18:17 -0800

    base project

---
+++ Mondrian.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.awt.*;
+
+public class Mondrian {
+    public void paintBasicMondrian(Color[][] pixels){
+        System.out.println("h");
+    }
+
+    public void paintComplexMondrian(Color[][] pixels){
+        System.out.println("b");
+    }
+    
+}
